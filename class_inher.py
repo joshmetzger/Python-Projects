13bec773236e584fcdc102b5ed9d0d0cc092b8cb
joshmetzger <75275288+joshmetzger@users.noreply.github.com>
2025-04-01 09:15:10 -0700

    class inheritance added

---
+++ class_inher.py
@@ -0,0 +1,18 @@
+class Building:
+    address = ''
+    sq_ft = 0
+    lease_terms = ''
+    rent_cost = ''
+
+
+class Residential(Building):
+    tentant_names: ''
+    number_tenants: 0
+    number_pets: 0
+
+
+class Commercial(Building):
+    business_type: ''
+    
+    
+    
